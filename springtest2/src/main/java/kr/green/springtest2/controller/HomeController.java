0e51ba0ac9e3f83d50e635b6b632fe216e5bf243
Gilson <dudrb8922@gmail.com>
2020-07-28 10:50:59 +0900

    스프링테스트2

---
+++ springtest2/src/main/java/kr/green/springtest2/controller/HomeController.java
@@ -0,0 +1,22 @@
+package kr.green.springtest2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView homeGet(ModelAndView mv) {
+		logger.info("URI:/main/home:GET");
+		mv.setViewName("/main/home");
+
+		return mv;
+	}
+}
---
+++ springtest2/src/main/java/kr/green/springtest2/dao/UserDao.java
@@ -0,0 +1,5 @@
+package kr.green.springtest2.dao;
+
+public class UserDao {
+
+}
